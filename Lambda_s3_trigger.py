de17b973fe01106e21d9bdc763237be7eb2b69b1
Viswateja Rayapaneni <viswatejaster@gmail.com>
2022-02-24 13:20:50 +0530

    Update Lambda_s3_trigger.py

--- Lambda_s3_trigger.py
+++ Lambda_s3_trigger.py
@@ -12,12 +12,14 @@ def lambda_handler(event, context):
     # Get bucket name and file name from s3
     bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
     s3_file_name = unquote_plus(event["Records"][0]["s3"]["object"]["key"])
-    
-    
+
     # using s3 client
     s3 = boto3.client('s3')
+    
     # Download file from s3 to lambda temp storage
     s3.download_file(bucket_name, s3_file_name, os.path.join("/tmp",s3_file_name))
+    
+    # Reading the json file from lambda local storage 
     files = glob.glob("/tmp"+"/*.json")
     with open(files[0], 'r') as f:
         data = json.load(f)
